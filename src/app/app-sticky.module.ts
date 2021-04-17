be0bb2970f44bfc261b9c193a05eb7b9f06c1317
LELO <nyaka.lelo@econocom.com>
2021-04-17 14:31:19 +0200

    debut implementation

---
+++ src/app/app-sticky.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { AngularStickyThingsModule } from "@w11k/angular-sticky-things";
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [
+      AngularStickyThingsModule,
+    ],
+    providers: [],
+  })
+  export class AppStickyModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppStickyModule } from './app-sticky.module';
+import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule,
+    BrowserAnimationsModule,
+    AppStickyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
