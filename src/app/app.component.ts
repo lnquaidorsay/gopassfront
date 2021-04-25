2c6568c25a4c4ca375a5128f4ba61111d36db8a3
LELO <nyaka.lelo@econocom.com>
2021-04-25 14:43:50 +0200

    adding ngx-chart module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,25 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('stickyMenu') menuElement: ElementRef;
+  //@ViewChild('stickyMenu') menuElement: ElementRef;
 
   sticky: boolean = false;
   elementPosition: any;
   title = 'gopassfront';
 
-  ngAfterViewInit(){
-    this.elementPosition = this.menuElement.nativeElement.offsetTop;
-  }
+  // ngAfterViewInit(){
+  //   this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  // }
 
-  @HostListener('window:scroll', ['$event'])
-  handleScroll(){
-    const windowScroll = window.pageYOffset;
-    if(windowScroll >= this.elementPosition){
-      this.sticky = true;
-    } else {
-      this.sticky = false;
-    }
-  }
+  // @HostListener('window:scroll', ['$event'])
+  // handleScroll(){
+  //   const windowScroll = window.pageYOffset;
+  //   if(windowScroll >= this.elementPosition){
+  //     this.sticky = true;
+  //   } else {
+  //     this.sticky = false;
+  //   }
+  // }
 
   scrollToElement($element): void {
     console.log("element clique : ",$element);
